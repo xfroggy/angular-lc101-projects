14f85aa3feae464e24e866788b7cecae64a4a0f6
Todd Smitala <tc.smitala@gmail.com>
2020-11-01 09:58:32 -0500

    Completed Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://c.files.bbci.co.uk/1AE0/production/_114208860_118764633_3516269255084630_2806544061200064216_o.jpg';
+  image3 = 'https://cdn2.lamag.com/wp-content/uploads/sites/6/2020/02/image.png';
 
   constructor() { }
 
