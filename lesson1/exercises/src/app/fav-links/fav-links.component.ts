32c0481878981aec65806be029c418c42accdebf
Todd Smitala <tc.smitala@gmail.com>
2020-11-01 19:46:27 -0500

    part 2 completed

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.launchcode.org/lc101', 'https://www.freecodecamp.org/'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
