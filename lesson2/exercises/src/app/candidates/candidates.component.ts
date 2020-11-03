15d5ebbeae4848cda4bc7e896d139aca7d65adf0
Todd Smitala <tc.smitala@gmail.com>
2020-11-03 09:02:00 -0500

    studio completed

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,7 +24,11 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew( crewMember){
+    if (!this.crew.includes(crewMember)) {
+      this.crew.push(crewMember);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
 
