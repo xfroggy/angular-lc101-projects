06c1b0b002dce43cd534f9d8dc817f7b27a34388
Todd Smitala <tc.smitala@gmail.com>
2020-11-02 23:32:41 -0500

    finished example

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { empty } from "rxjs";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+  movies = ["Toy Story", "The Shining", "Sleepless in Seattle", "The Martian"];
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  ngOnInit() {}
+
+  addMovie(newTitle: string) {
+    let msgError = "";
+    if (newTitle === "") {
+      msgError = "No film entered";
+    } else if (this.movies.includes(newTitle)) {
+      msgError = `${newTitle} is already on the list`;
+    } else {
+      this.movies.push(newTitle);
+    }
+    return msgError;
+  }
 }
